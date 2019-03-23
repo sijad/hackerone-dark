d6700eff0f5a674a571dd8e7b6003701bb41a04a
Sajjad Hashemian <wolaws@gmail.com>
2019-03-23 21:47:51 +0430

    add prettier

--- build.js
+++ build.js
@@ -2,35 +2,34 @@ const fs = require("fs");
 const parseCss = require("css").parse;
 
 //const colorRegex = /#(?:[0-9a-fA-F]{3}){1,2}/g;
-const colorRegex =
-/(#([0-9a-f]{3}){1,2}|(rgba|hsla)\(\d{1,3}%?(,\s?\d{1,3}%?){2},\s?(1|0?\.\d+)\)|(rgb|hsl)\(\d{1,3}%?(,\s?\d{1,3}%?\)){2})/ig;
+const colorRegex = /(#([0-9a-f]{3}){1,2}|(rgba|hsla)\(\d{1,3}%?(,\s?\d{1,3}%?){2},\s?(1|0?\.\d+)\)|(rgb|hsl)\(\d{1,3}%?(,\s?\d{1,3}%?\)){2})/gi;
 
 const colorsMap = {
-  '#ffffff': '#151515',
-  '#000000': '#c0c0c0',
-  '#aaaaaa': '#333',
-  '#f5f5f5': '#181818',
-  '#e5e5e5': '#222',
-  '#ececec': '#333',
-  '#c5c5c5': '#333',
-  '#f8f8f8': '#181818',
-  '#333333': '#d0d0d0',
-  '#d9ebf8': '#3F6A88',
-  '#fbf1f1': '#440000',
-  '#f5dddd': '#330000',
-  '#cccccc': '#333',
-  '#d6d6d6': '#333',
-  '#efefef': '#222',
-  '#c0c0c0': '#333',
-  '#999999': '#444',
-  '#ebebeb': '#333',
-  '#fff0b9': '#333',
+  "#ffffff": "#151515",
+  "#000000": "#c0c0c0",
+  "#aaaaaa": "#333",
+  "#f5f5f5": "#181818",
+  "#e5e5e5": "#222",
+  "#ececec": "#333",
+  "#c5c5c5": "#333",
+  "#f8f8f8": "#181818",
+  "#333333": "#d0d0d0",
+  "#d9ebf8": "#3F6A88",
+  "#fbf1f1": "#440000",
+  "#f5dddd": "#330000",
+  "#cccccc": "#333",
+  "#d6d6d6": "#333",
+  "#efefef": "#222",
+  "#c0c0c0": "#333",
+  "#999999": "#444",
+  "#ebebeb": "#333",
+  "#fff0b9": "#333",
 
-  'rgb(22, 56, 84)': 'rgb(33, 107, 165)',
+  "rgb(22, 56, 84)": "rgb(33, 107, 165)",
 
-  'rgba(32, 32, 32, 0.8)': 'rgba(150, 150, 150, 0.8)',
-  'rgba(0, 0, 0, 0.25)': 'rgba(100, 100, 100, 0.25)',
-  'rgba(255, 255, 255, 0.75)': 'rgba(10, 10, 10, 0.75)',
+  "rgba(32, 32, 32, 0.8)": "rgba(150, 150, 150, 0.8)",
+  "rgba(0, 0, 0, 0.25)": "rgba(100, 100, 100, 0.25)",
+  "rgba(255, 255, 255, 0.75)": "rgba(10, 10, 10, 0.75)"
 };
 
 const manualCss = `
@@ -54,18 +53,18 @@ a.topbar-navigation-link, i.icon-logo, input {
 `;
 
 const ignoredSelector = [
-  '.signed-in .topbar-navigation-link',
-  '.signed-in .topbar-navigation-link:hover',
-  '.signed-in .topbar-navigation-link.active',
-  '.signed-in .topbar-navigation-link:active',
-  '.signed-in .topbar-navigation-link:visited',
-  '.signed-in .topbar-logo',
-  '.signed-in .topbar-logo:hover',
-  '.label',
-  '.wizard-caption-heading.is-active',
+  ".signed-in .topbar-navigation-link",
+  ".signed-in .topbar-navigation-link:hover",
+  ".signed-in .topbar-navigation-link.active",
+  ".signed-in .topbar-navigation-link:active",
+  ".signed-in .topbar-navigation-link:visited",
+  ".signed-in .topbar-logo",
+  ".signed-in .topbar-logo:hover",
+  ".label",
+  ".wizard-caption-heading.is-active"
 ];
 
-fs.readdir('css-files', (err, files) => {
+fs.readdir("css-files", (err, files) => {
   if (err) {
     throw err;
   }
@@ -75,24 +74,26 @@ fs.readdir('css-files', (err, files) => {
     output += manualCss;
 
     files
-      .filter(file => (file.indexOf('.') !== 0))
+      .filter(file => file.indexOf(".") !== 0)
       .forEach(file => {
-        output += parse(fs.readFileSync(`./css-files/${file}`, 'utf8'));
+        output += parse(fs.readFileSync(`./css-files/${file}`, "utf8"));
       });
 
-    output += '}';
+    output += "}";
 
-    fs.writeFileSync('hackerone-dark.css', output);
+    fs.writeFileSync("hackerone-dark.css", output);
   }
 });
 
 function parse(data) {
-  const { stylesheet: { rules } } = parseCss(data);
+  const {
+    stylesheet: { rules }
+  } = parseCss(data);
 
   const colorRules = rules.map(processRules).filter(v => v && true);
 
   if (!colorRules || !colorRules.length) {
-    throw 'no color rules.';
+    throw "no color rules.";
   }
 
   const newRules = {};
@@ -104,10 +105,12 @@ function parse(data) {
           property,
           value,
           originalValue,
-          selectors: [],
+          selectors: []
         };
       }
-      selectors = selectors.filter(selector => ignoredSelector.indexOf(selector) === -1);
+      selectors = selectors.filter(
+        selector => ignoredSelector.indexOf(selector) === -1
+      );
       newRules[key].selectors.push(...selectors);
     });
   });
@@ -117,11 +120,13 @@ function parse(data) {
 
 function buildCss(rules) {
   const ruleValues = Object.values(rules);
-  let output = '';
+  let output = "";
   ruleValues.forEach(({ property, value, selectors }) => {
     if (selectors && selectors.length) {
-      value = value.replace('!important', '');
-      output += `\n${selectors.join(',')} {\n${property}: ${value} !important;\n}\n`;
+      value = value.replace("!important", "");
+      output += `\n${selectors.join(
+        ","
+      )} {\n${property}: ${value} !important;\n}\n`;
     }
   });
   return output;
@@ -133,33 +138,35 @@ function processRules({ declarations, rules, type, selectors }) {
     return rules.forEach(processRules);
   }
 
-  if (type === 'keyframes' || type === 'comment' || type === 'charset') {
+  if (type === "keyframes" || type === "comment" || type === "charset") {
     return;
   }
 
-  if (! declarations) {
-    throw 'no declarations';
+  if (!declarations) {
+    throw "no declarations";
   }
 
-  const properties = declarations.map(({ value, property }) => {
-    // make sure it's not comment.
-    if (value && colorRegex.test(value)) {
-      const newValue = value.replace(colorRegex, processColor);
-      if (newValue !== value) {
-        return {
-          property,
-          value: newValue,
-          originalValue: value,
-        };
+  const properties = declarations
+    .map(({ value, property }) => {
+      // make sure it's not comment.
+      if (value && colorRegex.test(value)) {
+        const newValue = value.replace(colorRegex, processColor);
+        if (newValue !== value) {
+          return {
+            property,
+            value: newValue,
+            originalValue: value
+          };
+        }
       }
-    }
-  }).filter(v => v && true);
+    })
+    .filter(v => v && true);
 
   if (properties && properties.length) {
     return {
       properties,
-      selectors,
-    }
+      selectors
+    };
   }
 }
 
@@ -175,8 +182,10 @@ function processColor(color) {
 
 function cleanupColor(color) {
   color = color.toLowerCase();
-  if (color.length === 4 && color.indexOf('#') === 0) {
-    return `#${color[1]}${color[1]}${color[2]}${color[2]}${color[3]}${color[3]}`;
+  if (color.length === 4 && color.indexOf("#") === 0) {
+    return `#${color[1]}${color[1]}${color[2]}${color[2]}${color[3]}${
+      color[3]
+    }`;
   }
   return color;
 }
